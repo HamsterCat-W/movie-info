ec6c5e7bc8062b13766083131ce8a19e36d7f1ce
HamsterCat-W <2756878164@qq.com>
2021-06-03 09:28:53 +0800

    更新了远程数据库

--- server/config.js
+++ server/config.js
@@ -4,5 +4,5 @@ module.exports = {
   secretKey: 's1N3tqyRo17EWPZ5oCszZyrZg4Iif4Fd',
   //   存在时间 1天
   expiresIn: '1d',
-  mongoUrl: 'mongodb://localhost/movie'
+  mongoUrl: 'mongodb://dajun:dajun5432@10.8.51.45:27017/se180106?retryWrites=true&authSource=admin'
 }
